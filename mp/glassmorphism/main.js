bf2e0d76c5a9a831f86f98ed8c4cbf17aeec0e9a
Aswath A B <aswathab437@gmail.com>
2025-09-13 10:00:09 +0530

    Updated portfolio v2

--- mp/glassmorphism/main.js
+++ mp/glassmorphism/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $('#card-number').on('keypress change blur', function () {
     $(this).val(function (index, value) {
       return value.replace(/[^a-z0-9]+/gi, '').replace(/(.{4})/g, '$1 ');
@@ -17,4 +18,25 @@ $('#card-number').on('keypress change blur', function () {
           newInput += '/';
           $(this).val(newInput);
       }
+=======
+$('#card-number').on('keypress change blur', function () {
+    $(this).val(function (index, value) {
+      return value.replace(/[^a-z0-9]+/gi, '').replace(/(.{4})/g, '$1 ');
+    });
+  });
+  
+  $('#card-number').on('copy cut paste', function () {
+    setTimeout(function () {
+      $('#card-number').trigger("change");
+    });
+  });
+  
+  $('#card-exp').on('input',function(){
+      var curLength = $(this).val().length;
+      if(curLength === 2){
+         var newInput = $(this).val();
+          newInput += '/';
+          $(this).val(newInput);
+      }
+>>>>>>> 30df9f6 (Updated portfolio v2)
   }); 
